ad53fbeae507c273893d949c3b954a0449e9f788
Yripjar <rpriyanshu22@gmail.com>
2026-03-06 10:38:15 +0530

    UC8: Use HashMap for character pattern storage and rendering

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,81 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
+    /**
+     * Build the character pattern map
+     */
+    public static Map<Character, String[]> buildCharacterPatternMap() {
 
-        String[] banner = new String[7];
-            banner[0] = String.join(" ", "   ***    ", "   ***    ", "  ******  ", "  *****  ");
-            banner[1] = String.join(" ", "  **  **  ", "  **  **  ", "  **   ** ", " **   ** ");
-            banner[2] = String.join(" ", " **    ** ", " **    ** ", "  **   ** ", " **      ");
-            banner[3] = String.join(" ", " **    ** ", " **    ** ", "  ******  ", "  *****  ");
-            banner[4] = String.join(" ", " **    ** ", " **    ** ", "  **      ", "      ** ");
-            banner[5] = String.join(" ", "  **  **  ", "  **  **  ", "  **      ", " **   ** ");
-            banner[6] = String.join(" ", "   ***    ", "   ***    ", "  **      ", "  *****  ");
+        Map<Character, String[]> patternMap = new HashMap<>();
 
-        for (String line : banner) {
+        patternMap.put('O', new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        });
+
+        patternMap.put('P', new String[]{
+                " ******  ",
+                " **   ** ",
+                " **   ** ",
+                " ******  ",
+                " **      ",
+                " **      ",
+                " **      "
+        });
+
+        patternMap.put('S', new String[]{
+                "  *****  ",
+                " **      ",
+                " **      ",
+                "  *****  ",
+                "      ** ",
+                "      ** ",
+                " *****   "
+        });
+
+        return patternMap;
+    }
+
+    /**
+     * Render banner message
+     */
+    public static void renderBanner(String message, Map<Character, String[]> patternMap) {
+
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : message.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append(" ");
+                }
+            }
+
             System.out.println(line);
         }
-    }   
+    }
+
+    /**
+     * Main Method
+     */
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildCharacterPatternMap();
+
+        String message = "OOPS";
+
+        renderBanner(message, patternMap);
+    }
 }
